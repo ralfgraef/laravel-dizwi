2df626506a72dec07ec81202c641b3c0d7033cc3
ralfgraef <graef.ralf@gmail.com>
2019-03-29 20:38:06 +0100

    add change to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
