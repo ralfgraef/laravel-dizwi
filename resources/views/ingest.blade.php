e7b29741418e1700d2564408e52e24d1ac373c09
ralfgraef <graef.ralf@gmail.com>
2019-04-08 18:14:15 +0200

    layout changes

--- resources/views/ingest.blade.php
+++ resources/views/ingest.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
            <h2>Ingest</h2>
         </div>
     </div>
